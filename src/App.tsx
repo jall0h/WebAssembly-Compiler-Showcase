458308429ccd345a74f286ee00c06e7c66ed9244
Abu-Bakarr Jalloh <k22010427@kcl.ac.uk>
2025-11-21 16:25:35 +0000

    import tailwind and create base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const compileCode = () => {
+    console.log("Compiling code...");
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="flex gap-4">
+        <div>
+          <textarea className="border" />
+        </div>
+        <button
+          onClick={() => {
+            compileCode();
+          }}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Compile
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div>
+          <textarea className="border" />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
